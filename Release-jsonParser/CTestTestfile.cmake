87bbebb40f8ef95a57485416b54608900b8f3251
andreiploticin <andreiploticin@gmail.com>
2021-03-01 10:07:02 +0300

    release folder removed

--- Release-jsonParser/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/cpp/jsonParser
-# Build directory: C:/dev/cpp/jsonParser/Release-jsonParser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("tests")
-subdirs("example")
--- Release-jsonParser/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/cpp/jsonParser/tests
-# Build directory: C:/dev/cpp/jsonParser/Release-jsonParser/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(JsonTests "C:/dev/cpp/jsonParser/Release-jsonParser/tests/JsonTests.exe")
-set_tests_properties(JsonTests PROPERTIES  _BACKTRACE_TRIPLES "C:/dev/cpp/jsonParser/tests/CMakeLists.txt;24;add_test;C:/dev/cpp/jsonParser/tests/CMakeLists.txt;0;")
